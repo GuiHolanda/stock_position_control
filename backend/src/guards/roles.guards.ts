53ca469a73f9a65f7eda08d2f3621d6a25254f65
C5366084 <guilherme.holanda@sap.com>
2024-05-28 14:22:07 -0300

    feature: create role and userId decorators and role guard

---
+++ backend/src/guards/roles.guards.ts
@@ -0,0 +1,38 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { ROLES_KEY } from '../decorators/role.decorator';
+import { UserType } from '../user/enum/user_type.enum';
+import { LoginPayload } from '../auth/dtos/loginPayload.dto';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+    private readonly jwsService: JwtService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<UserType[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const { authorization } = context.switchToHttp().getRequest().headers;
+
+    const loginPayload: LoginPayload | undefined = await this.jwsService
+      .verifyAsync(authorization, {
+        secret: process.env.JWT_SECRET,
+      })
+      .catch(() => undefined);
+
+    if (!loginPayload) {
+      return false;
+    }
+
+    return requiredRoles.some((role) => role === loginPayload.typeUser);
+  }
+}
