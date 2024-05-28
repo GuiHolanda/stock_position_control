7556bc4f539d83755342639e5fbd686ba4ac13c9
C5366084 <guilherme.holanda@sap.com>
2024-05-28 14:24:18 -0300

    feature: create auth module with its service and controller

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDTO } from './dtos/login.dto';
+import { ReturnLoginDTO } from './dtos/returnLogin.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @UsePipes(ValidationPipe)
+  @Post()
+  async login(@Body() loginDTO: LoginDTO): Promise<ReturnLoginDTO> {
+    return await this.authService.login(loginDTO);
+  }
+}
---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UserEntity } from '../user/entities/user.entity';
+import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { ReturnUserDTO } from '../user/dtos/returnUser.dto';
+import { LoginPayload } from './dtos/loginPayload.dto';
+import { validatePassword } from '../utils/password';
+import { LoginDTO } from './dtos/login.dto';
+import { ReturnLoginDTO } from './dtos/returnLogin.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(loginDTO: LoginDTO): Promise<ReturnLoginDTO> {
+    const user: UserEntity | undefined = await this.userService
+      .getUserByEmail(loginDTO.email)
+      .catch(() => undefined);
+
+    const isMatch = await validatePassword(
+      loginDTO.password,
+      user?.password || '',
+    );
+
+    if (!user || !isMatch) {
+      throw new NotFoundException('Invalid email or password');
+    }
+
+    return {
+      accessToken: this.jwtService.sign({ ...new LoginPayload(user) }),
+      user: new ReturnUserDTO(user),
+    };
+  }
+}
